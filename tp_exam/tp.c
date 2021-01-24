816d32538048dd29b8451d89079be35fc5f2a841
Tristan Pinaudeau <tristan@tic.sh>
2021-01-24 11:03:22 +0100

    start exam

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -4,6 +4,29 @@
 
 extern info_t *info;
 
-void tp()
-{
+void user1() {
+  debug("=== ENTER USER 1 ===\n");
+  // TODO: increment shared counter
+  // TODO: infinit loop
+}
+
+void user2() {
+  debug("=== ENTER USER 2 ===\n");
+  // TODO: syscall to display shared counter
+  // TODO: infinit loop
+}
+
+void userland() {
+  debug("== ENTER USERLAND ==\n");
+
+  // TODO: launch task 1
+  // TODO: launch task 2
+
+  while (1) {}
+}
+
+void tp() {
+  // TODO: configure segmentation
+  // TODO: configure pagination (identity mapping)
+  // TODO: configure interruption
 }
